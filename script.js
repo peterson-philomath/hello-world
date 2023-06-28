54e932c360b9332a415f5142e8ea4bbf18e2b7ab
Philip Peterson <philip.peterson@philomath.k12.or.us>
2023-06-27 21:39:11 -0700

    Adding JavaScript

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// const userName = prompt("What is your name?");
+// console.log(userName);
+
+// document.write("Hi " + userName + ", welcome to my Page!");
+
+
+// function greeting(){
+//     const userName = prompt("What is your name?");
+// console.log(userName);
+
+// document.write("Hi " + userName + ", welcome to my Page!");
+// }
