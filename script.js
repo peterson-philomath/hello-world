120aa952c950e747a08c70e04ee4d21fedeed73e
Philip Peterson <philip.peterson@philomath.k12.or.us>
2023-06-28 20:54:32 -0700

    loops

--- script.js
+++ script.js
@@ -1,53 +1,81 @@
 'use strict';
 
-
+let theirName = getName();
+const projects = 7;
+let projectRequested = 0;
+let pictures = ["boardgamesection.jpg", "Boardgametop.jpg", "Boardgametable.jpg", "ChargeStation.jpg", "MedicineCabinet.jpg", "PatioCover.jpg", "WindowSeat.jpg"];
 
 function getName(){
     const userName = prompt("What is your name?");
-    console.log(userName);
     return userName;
 }
 
 console.log(theirName);
 
 function greetUser(){
-    if(theirName == ""){
+    while(theirName == ""){
       theirName = prompt("Please let me know what to call you?");
-      console.log(theirName);
+      console.log(theirName.toLowerCase());
     }
-    if (theirName == "Kassie"){
+    if (theirName.toLowerCase() == "kassie"){
         document.write("Hi Teach, welcome to my Page!");
     } else{
         document.write("Hi " + theirName + ", welcome to my Page!");
     }
-    alert("Please vote for your favorite project.");
 }
 
-const table = ['Table', "table", 'Board Game Table', "board game table", "boardgame table", "BoardGame Table", "Boardgame table"];
-const cabinet = ['Medicine Cabinet', 'Cabinet', 'cabinet', 'medicine cabinet'];
-const cStation = ['Charging Station','charging station', 'charger'];
-const seat = ['Window seat', 'window seat', 'Window seat', 'seat', 'window'];
-const backyard = ['Patio Cover', 'backyard', 'Backyard', 'Patio cover', 'patio cover', 'garden box', 'Patio Cover and Garden Box', 'Garden Box', 'Patio cover and garden box'];
+function projectRequest(){
+    projectRequested = prompt("How many celebrations would you like to see? Between 1-7");
+    console.log(projectRequested);
+    while( projectRequested > projects || projectRequested <= 0){
+        projectRequested = prompt("Try Again. How many projects would you like to see? Between 1-7");
+    }
+    for (let i = 0; i < projectRequested; i++){
+        document.write("<img src='celebration-will-smith.gif' />");
+    }
+    
+}
+
+// for( i = 0, i < projectsRequested; i++){
+//     document.write(pictures[i]);
+// }
+
+
+const table = ["table", "board game table", "boardgame table"];
+const cabinet = ['cabinet', 'medicine cabinet'];
+const cStation = ['charging station', 'charger'];
+const seat = ['window seat','seat', 'window'];
+const backyard = ['backyard', 'patio cover', 'garden box', 'patio cover and garden box'];
+
+
+
 
 // Function Declaration
 function projectVote() {
-      const vote = prompt("What is your favorite project? Use the project name with capitals.");
+      let vote = prompt("What is your favorite project? Use the project name with capitals.");
       console.log(vote);  //log vote in console
-      if(vote==""){
-        alert("Your vote is important");
-        projectVote();
+      while(vote==""){
+        vote = prompt("Your vote is important, Pick your favorite.");
       }
-      if (table.includes(vote)) {
+      if (table.includes(vote.toLowerCase)) {
         document.write("Thanks, I am very proud of that one.");
-      } else if(cabinet.includes(vote)) {
+        projectRequest();
+      } else if(cabinet.includes(vote.toLowerCase)) {
         document.write("Gotta look good in the mirror");
-      } else if (cStation.includes(vote)){
+        projectRequest();
+      } else if (cStation.includes(vote.toLowerCase)){
         document.write("Organization is very important");
-      } else if(seat.includes(vote)){
+        projectRequest();
+      } else if(seat.includes(vote.toLowerCase)){
         document.write("That improved my living room so much");
-      }else if(backyard.includes(vote)){
+        projectRequest();
+      }else if(backyard.includes(vote.toLowerCase)){
         document.write("It makes the outside that much more enjoyable.");
+        projectRequest();
       } else{
-    document.write("Thanks for your vote");
+        document.write("Thanks for your vote");
+        projectRequest();
     }
 }
+
+      
